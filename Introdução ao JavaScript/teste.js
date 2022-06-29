0bbaf110daa2666c1bff672e4f5b2aa790b0b443
mdsjr <mds.junior87@gmail.com>
2022-06-29 20:58:39 -0300

    Into git commit -m

---
+++ Introdução ao JavaScript/teste.js
@@ -0,0 +1,12 @@
+//console.log("Hello world!");
+
+function returnEvenValues(array) {
+    for (let i = 0; i <array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i])
+        }
+        }
+}
+console.log(evenNums);
+
+let array = [1,2,3,4,5,6,7,8]
